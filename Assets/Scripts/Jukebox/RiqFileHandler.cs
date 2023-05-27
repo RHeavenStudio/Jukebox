2c3f82ba8c62da744ede83d0ece7d37f93c107c9
minenice55 <star.elementa@gmail.com>
2023-05-26 23:24:40 -0400

    prelim riq packing / writing functions

--- Assets/Scripts/Jukebox/RiqFileHandler.cs
+++ Assets/Scripts/Jukebox/RiqFileHandler.cs
@@ -98,5 +98,63 @@ namespace Jukebox
                 yield return null;
             }
         }
+
+        /// <summary>
+        /// writes a beatmap to the "remix.json" file in the temporary cache
+        /// </summary>
+        /// <param name="beatmap">RiqBeatmap to serialize</param>
+        public static void WriteRiq(RiqBeatmap beatmap)
+        {
+            string jsonPath = tmpDir + "remix.json";
+            string json = beatmap.Serialize();
+            File.WriteAllText(jsonPath, json, System.Text.Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// copies a song file to the temporary cache, renaming it to "song.bin"
+        /// </summary>
+        /// <param name="songPath">path to a song file</param>
+        public static void WriteSong(string songPath)
+        {
+            if (songPath == string.Empty || songPath == null) throw new System.ArgumentNullException("path", "path cannot be null or empty");
+            if (!File.Exists(songPath)) throw new System.IO.FileNotFoundException("path", $"RIQ file does not exist at path {songPath}");
+
+            // check if songPath is a valid audio file
+            try
+            {
+                using (var www = UnityWebRequestMultimedia.GetAudioClip("file://" + songPath, AudioType.UNKNOWN))
+                {
+                    www.SendWebRequest();
+                    if (www.result == UnityWebRequest.Result.ConnectionError)
+                    {
+                        Debug.Log($"error loading song: {www.error}");
+                        throw new System.Exception($"error loading song: {www.error}");
+                    }
+                }
+            }
+            catch (System.Exception e)
+            {
+                Debug.LogError($"Error loading song: {e.Message}");
+                throw e;
+            }
+
+            string songDest = tmpDir + "song.bin";
+            File.Copy(songPath, songDest, true);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="destPath"></param>
+        public static void PackRiq(string destPath)
+        {
+            if (tmpDir == string.Empty || tmpDir == null) throw new System.ArgumentNullException("path", "temporary directory cannot be null or empty");
+            if (destPath == string.Empty || destPath == null) throw new System.ArgumentNullException("path", "destination path cannot be null or empty");
+
+            string jsonPath = tmpDir + "remix.json";
+            if (!File.Exists(jsonPath)) throw new System.IO.FileNotFoundException("path", $"riq chart file does not exist at path {jsonPath}, was an RIQ file properly extracted?");
+
+            ZipFile.CreateFromDirectory(tmpDir, destPath);
+        }
     }
 }
